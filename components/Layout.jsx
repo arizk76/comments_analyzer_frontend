ba3bf3fa40ce1454d013d8d2e029a7cd9c66f0d5
Ahmed <arizkcisco@gmail.com>
2021-08-23 21:11:36 +0200

    extra meta tags for testing

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,12 +6,25 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>Comment Analyzer Application</title>
-        <meta charSet='utf-8' />
+        <meta
+          name='description'
+          content='Comments Analyzer - Find out what they really think'
+        />
+        <meta charset='UTF-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
         <meta
           name='viewport'
-          content='width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover'
+          content='width=device-width, initial-scale=1.0, minimal-ui,maximum-scale=1,user-scalable=0,viewport-fit=cover'
         />
+
+        <meta meta name='apple-mobile-app-capable' content='yes' />
         <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-title' content='Comment Analyzer' />
+        <meta name='robots' content='index, follow' />
+        <link
+          rel='canonical'
+          href='https://comments-analyzer-frontend.vercel.app/'
+        />
       </Head>
 
       <main className='flex flex-col min-h-screen content-between '>
