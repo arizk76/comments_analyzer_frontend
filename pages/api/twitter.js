8414d12dee14bf803bbb7e00e9f4a848ae02c775
Ahmed <arizkcisco@gmail.com>
2021-08-23 18:55:46 +0200

    cors for twitter api

--- pages/api/twitter.js
+++ pages/api/twitter.js
@@ -1,4 +1,29 @@
+import Cors from 'cors';
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['POST'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
+  // Rest of the API logic
   const userInputURL = req.query.url;
   const userSource = req.query.source;
 
@@ -17,10 +42,7 @@ async function handler(req, res) {
   let response, responseJson;
 
   try {
-    response = await fetch(
-      'http://commentsanalyzer.herokuapp.com/ca/twitter/v1/results',
-      options
-    );
+    response = await fetch(process.env.TWITTER_API_URL, options);
     // console.log(response);
     if (!response.ok) {
       const message = `An error has occurred while fetch request, No response: ${response.status}`;
